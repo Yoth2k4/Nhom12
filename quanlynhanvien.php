b87c24aa44353868aec77c61d89cff6ed5076f7b
ptlong2004 <long3122004@gmail.com>
2025-04-13 20:30:45 +0700

    Update quanlynhanvien.php

--- quanlynhanvien.php
+++ quanlynhanvien.php
@@ -5,7 +5,6 @@ if (!(isset($_SESSION['role']) && $_SESSION['role'] == "admin")) {
 ?>
     <!DOCTYPE html>
     <html lang="en">
-
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
